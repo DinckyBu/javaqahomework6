31a13b43ee2c768cf6551ccad6b824f98cd0ecf7
Екатерина Кобелева <kobelevaekaterina09@gmail.com>
2023-04-30 21:45:04 +0300

    Added project

---
+++ src/main/java/ru/netology/statsService/StatsService.java
@@ -0,0 +1,73 @@
+package ru.netology.statsService;
+import java.util.Arrays;
+
+public class StatsService {
+
+    //1 Сумма всех продаж
+    public static int sum(int[] args) {
+        int sales[] = new int[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int sum = Arrays.stream(sales).sum();
+        return sum;
+    }
+
+    //2 Средняя сумма всех продаж
+    public static int averageSales(int[] args) {
+        int sales[] = new int[]{8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+        int sum = Arrays.stream(sales).sum();
+        int i = sales.length;
+        int averageSales = sum / i;
+
+        return averageSales;
+
+    }
+
+    //3 Месяц с максимальным числом продаж
+    public int getMaxSales(int[] sales) {
+        int maxSales = sales[0];
+        for (int t : sales) {
+            if (t < maxSales) {
+                maxSales = t + 1;
+            }
+        }
+        return maxSales;
+    }
+
+
+    //4 Месяц с минимальным числом продаж
+    public int getMinSales(int[] sales) {
+        int minSales = sales[0];
+        for (int t : sales) {
+            if (t < minSales) {
+                minSales = t + 1;
+            }
+        }
+        return minSales;
+    }
+
+    //5 количество месяцев, в которых продажи были ниже среднего;
+    public int belowAverage(int[] sales) {
+        int averageSales = Arrays.stream(sales).sum() / sales.length;
+        int month = 0;
+        for(int x = 0; x < sales.length; x++)
+        if (sales[x] < averageSales){
+            month = month + 1;
+        }
+            else {
+            month = month;
+        }
+        return month;
+        }
+    //5 количество месяцев, в которых продажи были выше среднего;
+    public int afterAverage(int[] sales) {
+        int averageSales = Arrays.stream(sales).sum() / sales.length;
+        int month = 0;
+        for(int x = 0; x < sales.length; x++)
+            if (sales[x] > averageSales){
+                month = month + 1;
+            }
+            else {
+                month = month;
+            }
+        return month;
+    }
+}
---
+++ src/test/java/ru/netology/statsService/StatsServiceTest.java
@@ -0,0 +1,90 @@
+package ru.netology.statsService;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class StatsServiceTest {
+
+    //1 Сумма всех продаж
+    @Test
+    public void amountSales() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedAmountSales = 180;
+        int actualAmountSales = service.sum(sales);
+
+        Assertions.assertEquals(expectedAmountSales, actualAmountSales);
+        System.out.println(expectedAmountSales + " == ? == " + actualAmountSales);
+    }
+
+    //2 Средняя сумма всех продаж
+    @Test
+    public void averageSales() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedAverageSales = 15;
+        int actualAverageSales = service.averageSales(sales);
+
+        Assertions.assertEquals(expectedAverageSales, actualAverageSales);
+        System.out.println(expectedAverageSales + " == ? == " + actualAverageSales);
+    }
+
+    //3 Месяц с максимальным числом продаж
+    @Test
+    public void shouldFindMaxSales() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedAverageSales = 8;
+        int actualAverageSales = service.getMaxSales(sales);
+
+        Assertions.assertEquals(expectedAverageSales, actualAverageSales);
+        System.out.println(expectedAverageSales + " == ? == " + actualAverageSales);
+    }
+
+    //4 Месяц с минимальным числом продаж
+    @Test
+    public void shouldFindMinSales() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedAverageSales = 8;
+        int actualAverageSales = service.getMinSales(sales);
+
+        Assertions.assertEquals(expectedAverageSales, actualAverageSales);
+        System.out.println(expectedAverageSales + " == ? == " + actualAverageSales);
+    }
+
+    //5 количество месяцев, в которых продажи были ниже среднего;
+    @Test
+    public void findTheNumberOfMonthsWithSalesBelowAverage() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedAverageSales = 5;
+        int actualAverageSales = service.belowAverage(sales);
+
+        Assertions.assertEquals(expectedAverageSales, actualAverageSales);
+        System.out.println(expectedAverageSales + " == ? == " + actualAverageSales);
+    }
+    //6 количество месяцев, в которых продажи были выше среднего;
+    @Test
+    public void findTheNumberOfMonthsWithSalesAfterAverage() {
+        StatsService service = new StatsService();
+
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedAverageSales = 5;
+        int actualAverageSales = service.afterAverage(sales);
+
+        Assertions.assertEquals(expectedAverageSales, actualAverageSales);
+        System.out.println(expectedAverageSales + " == ? == " + actualAverageSales);
+    }
+}
+
